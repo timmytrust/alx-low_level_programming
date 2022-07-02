be726e832af0814b948b585be021acd202ab63c7
timmytrust <timmytrust5@gmail.com>
2022-07-02 13:11:18 -0700

    low level

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,11 @@
 int main(void)
 {
 
-printf("Side of a char:%d byte(s)\n",sizeof(char));
-printf("Side of an int:%d byte(s)\n",sizeof(int));
-printf("Side of a long int:%d byte(s)\n",sizeof(long long int));
-printf("Side of a float:%d byte(s)\n",sizeof(float));
+printf("Size of a char:%d byte(s)\n",sizeof(char));
+printf("Size of an int:%d byte(s)\n",sizeof(int));
+printf("Size of a long int:%d byte(s)\n",sizeof(long int));
+printf("Size of a long long int:%d byte(s)\n",sizeof(long long int));
+printf("Size of a float:%d byte(s)\n",sizeof(float));
 return (0);
 
 }
