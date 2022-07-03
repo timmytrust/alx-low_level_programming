fd2ec05b3239dbe738d4cd0982af34a045ad9425
timmytrust <timmytrust5@gmail.com>
2022-07-03 12:03:50 -0700

    low level

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - prints all single digit numbers of base 10 starting from 0
+*followed by a new line.
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+
+		putchar(n);
+
+		if (n != 57)
+		{
+
+			putchar(',');
+
+			putchar('');
+
+		}
+
+
+
+	}
+
+	putchar('\n');
+
+		return (0);
+}
