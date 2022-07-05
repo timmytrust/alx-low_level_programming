b0493a042723c97b149312040657ed0f69228d44
timmytrust <timmytrust5@gmail.com>
2022-07-05 14:08:56 -0700

    low level

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+*print_alphabet_x10 -> print alphabets ten times 
 *main -> prints 10 times the alphabet, in lowercase, followed by a new line.
 *Return: Always 0 (Success)
 */
