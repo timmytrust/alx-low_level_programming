ed615be6f3778b48ead7409a4aa62a073802e344
timmytrust <timmytrust5@gmail.com>
2022-07-05 15:35:03 -0700

    low level

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+*add - adds two numbers
+*@a: int to be
+*@b: int to be
+*Return: sum of a and b
+*/
+
+
+
+int add(int a, int b)
+{
+
+
+
+	int c;
+
+
+
+	c = a + b;
+
+	return (c);
+
+
+
+}
+
+
