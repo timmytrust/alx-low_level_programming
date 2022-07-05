254d54c98b8d5e4ad529b67d393b5137478bc0fb
timmytrust <timmytrust5@gmail.com>
2022-07-05 15:05:15 -0700

    low level

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*_abs - return absolute value of a number
+*@n: parameter
+*Return: value of number
+*/
+
+
+
+int _abs(int n)
+{
+
+
+
+	if (n < 0)
+		n = -1;
+
+	return (n);
+
+
+
+}
+
+
+
+
