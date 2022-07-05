949b2aa48b2e57dce06dfba71be7df28a930b9f3
timmytrust <timmytrust5@gmail.com>
2022-07-05 09:34:23 -0700

    low level

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+
+
+/**
+ *
+ * *main - prints alphabets
+ *
+ * *Return: Always 0 (Success)
+ *
+ * */
+
+
+
+void print_alphabet(void)
+{
+
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+
+		_putchar(j);
+
+	}
+
+	_putchar('\n');
+
+}
