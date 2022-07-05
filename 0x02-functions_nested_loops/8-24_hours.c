1c894e8b2124730399d9e8b4c06d032474a7447a
timmytrust <timmytrust5@gmail.com>
2022-07-05 15:27:56 -0700

    low level

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+
+
+/**
+*jack_bauer - prints last digit
+*starting from 00:00
+*Return: the last digit
+*/
+
+
+
+void jack_bauer(void);
+{
+
+
+
+	int i;
+
+
+
+	int j;
+
+
+
+	i = 0;
+
+
+
+	while (i < 24)
+	{
+
+		j = 0;
+
+		while (j < 60)
+		{
+
+			_putchar((i / 10) + '0');
+
+			_putchar((i % 10) + '0');
+
+			_putchar(':');
+
+			_putchar((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
+
+			_putchar('\n');
+
+			j++;
+
+		}
+
+		i++;
+
+	}
+
+
+
+}
