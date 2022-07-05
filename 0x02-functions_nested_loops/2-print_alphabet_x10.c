f7eac74aecb59624f920833e65694fc69b7580b8
timmytrust <timmytrust5@gmail.com>
2022-07-05 13:40:42 -0700

    low level

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,34 +5,24 @@
 *Return: Always 0 (Success)
 */
 
-
-
 void print_alphabet_x10(void)
 {
+int j;
 
+int x;
 
+for (x = 0; x < 10; x++)
+{
 
-	int j;
+ for (j = 'a'; j <= 'z'; j++)
+ {
 
-	int x;
+ _putchar(j);
 
+ }
 
+ _putchar('\n');
 
-	for (x = 0; x < 10; x++)
-	{
-
-		 for (j = 'a'; j <= 'z'; j++)
-		 {
-
-			 _putchar(j);
-
-
-
-		 }
-		 _putchar('\n');
-	}
-
-
-		 
+}
 
 }
