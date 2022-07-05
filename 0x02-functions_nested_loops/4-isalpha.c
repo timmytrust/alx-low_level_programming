bcd765ad584d1fe95abb807e64f803e8c2ce8803
timmytrust <timmytrust5@gmail.com>
2022-07-05 14:41:37 -0700

    low level

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * * _isalpha - check if the character is lower
+ *
+ * *@c: a character to be checked on
+ *
+ * *Return: 1 and 0 depending on condition
+ *
+ * */
+
+
+
+int _isalpha(int c)
+{
+
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
