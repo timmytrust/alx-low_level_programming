d633ca664e9b9ef14a0177541f46ee4a1217d144
timmytrust <timmytrust5@gmail.com>
2022-07-05 14:55:36 -0700

    low level

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,7 @@ int print_sign(int n)
 
 
 
-	if (n > 0)
+	if ( n > 0)
 	{
 
 		_putchar('+');
@@ -22,7 +22,7 @@ int print_sign(int n)
 
 	}
 
-	else if (n == 0)
+	else if ( n == 0)
 	{
 
 		_putchar('0');
