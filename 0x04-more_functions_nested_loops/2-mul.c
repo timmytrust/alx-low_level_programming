74f7d9c100c8d5198a731b848b884034efd706ec
timmytrust <timmytrust5@gmail.com>
2022-07-07 09:40:22 -0700

    low level

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,15 +3,10 @@
 
 
 /**
- *
  * mul - Multiplies two integers
- *
  * @a: The first number to multiply
- *
  * @b: The second number to multiply with first number
- *
  * Return: The result of multiplication between x param and y param
- *
 */
 
 int mul(int a, int b)
