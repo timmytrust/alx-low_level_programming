f08ba74fad7f4afe45bf9d533320f47d0301fcf4
timmytrust <timmytrust5@gmail.com>
2022-07-13 11:29:34 -0700

    low level

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * _strcmp - Compares two strings
  * @s1: The first string
@@ -10,83 +8,44 @@
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-
 	int a = 0, b = 0, c = 0, r = 0, lim;
 
-
-
 	while (s1[a])
-
 	{
-
 		a++;
-
 	}
-
-
 
 	while (s2[b])
-
 	{
-
 		b++;
-
 	}
 
-
-
 	if (a <= b)
-
 	{
-
 		lim = a;
-
 	}
 
 	else
-
 	{
-
 		lim = b;
-
 	}
 
-
-
 	while (c <= lim)
-
 	{
-
 		if (s1[c] == s2[c])
-
 		{
-
 			c++;
-
 			continue;
-
 		}
 
 		else
-
 		{
-
 			r = s1[c] - s2[c];
 
 			break;
-
 		}
-
-
-
 		c++;
-
 	}
-
-
-
 	return (r);
-
 }
