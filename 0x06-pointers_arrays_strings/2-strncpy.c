22e9125cb8dd636fd14214d8f2759cb743fb6611
timmytrust <timmytrust5@gmail.com>
2022-07-13 11:31:31 -0700

    low level

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,45 +13,24 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-
 	int a = 0, b = 0;
 
-
-
 	while (src[b])
-
 	{
-
 		b++;
-
 	}
-
-
 
 	while (a < n && src[a])
-
 	{
-
 		dest[a] = src[a];
-
 		a++;
-
 	}
-
-
 
 	while (a < n)
-
 	{
-
 		dest[a] = '\0';
-
 		a++;
-
 	}
 
-
-
 	return (dest);
-
 }
