206c7228110685587ca8b4c41dac96e5de1f011b
timmytrust <timmytrust5@gmail.com>
2022-07-13 11:59:40 -0700

    low level

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,6 @@
  */
 
 char *leet(char *s)
-
 {
 
 	int a = 0, b = 0, l = 5;
@@ -18,48 +17,23 @@ char *leet(char *s)
 
 	char n[5] = {'4', '3', '0', '7', '1'};
 
-
-
 	while (s[a])
-
 	{
-
 		b = 0;
 
-
-
 		while (b < l)
-
 		{
-
 			if (s[a] == r[b] || s[a] - 32 == r[b])
 
 			{
-
 				s[a] = n[b];
-
 			}
 
-
-
 			b++;
-
 		}
 
-
-
 		a++;
-
 	}
 
-
-
 	return (s);
-
-}
-
-
-
-return (s);
-
 }
