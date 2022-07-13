74904dbfa0d66203ef8e5c7355a8f32cf64030d7
timmytrust <timmytrust5@gmail.com>
2022-07-13 11:19:05 -0700

    low level

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,18 +3,12 @@
 
 
 /**
- *
  * _strncpy - Copy a string
- *
  * @dest: The destination value
- *
  * @src: The source value
- *
  * @n: The copy limit
- *
  * Return: char value
- *
- * */
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 
