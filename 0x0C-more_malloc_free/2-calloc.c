c98d6860358c35bff23322ee5a66b23a62321c8e
timmytrust <timmytrust5@gmail.com>
2022-07-29 03:45:56 -0700

    more malloc task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdlib.h>
-
-
 
 /**
  * _calloc - ...
@@ -12,32 +9,21 @@
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
 
 	int i = 0, l = 0;
 
 	char *p;
 
-
-
 	if (nmemb == 0 || size == 0)
-
 		return (NULL);
-
-
 
 	l = nmemb * size;
 
 	p = malloc(l);
 
-
-
 	if (p == NULL)
-
 		return (NULL);
-
-
 
 	while (i < l)
 
@@ -48,3 +34,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		i++;
 
 	}
+}
